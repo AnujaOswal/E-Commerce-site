3e1fcfadff00ced4646fa7d800cb07d83363b5a3
AnujaOswal <Oswalanujak@gmail.com>
2022-04-12 12:36:45 +0530

    packages added

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import colors from 'colors'
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URL,
+            {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+
+    } catch(error){
+        console.log(`Error: ${error.message}`.red.underline.bold)
+        process.exit(1)
+    }
+}
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app =express()
 
@@ -13,7 +17,9 @@ app.get('/api/products',(req,res) => {
     res.json(products)
 })
 app.get('/api/products/:id',(req,res) => {
-    const product = products.find((p) => p._id === req.params.id)
+    const product = products.find(
+        (p) => p._id === req.params.id
+        )
     res.json(product)
 })
 
@@ -21,4 +27,4 @@ const PORT = process.env.PORT || 5000
 
 
 app.listen(PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
